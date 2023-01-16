49d70af584e0070a25560dfbef245938215aa125
devlin <webmasterdevlin@gmail.com>
2023-01-17 00:38:45 +0100

    update

--- src/tests/HeroesPage.test.ts
+++ src/tests/HeroesPage.test.ts
@@ -1,32 +1,8 @@
 import { expect, test } from '@playwright/test';
 
-// test.beforeEach(async ({ page }) => {
-//   // mock heroes http request
-//   await page.route('http://localhost:3000/api/heroes', route => {
-//     route.fulfill({
-//       status: 200,
-//       body: JSON.stringify([
-//         {
-//           id: '7ggew732dw',
-//           firstName: 'Barry',
-//           lastName: 'Allen',
-//           house: 'DC',
-//           knownAs: 'Flash',
-//         },
-//         {
-//           id: '1ggew732dw',
-//           firstName: 'Scott',
-//           lastName: 'Summer',
-//           house: 'Marvel',
-//           knownAs: 'Cyclopes',
-//         },
-//       ]),
-//     });
-//   });
-// });
-
-test('Success heroes HTTP fetch', async ({ page }) => {
-  await page.route('http://localhost:5173/api/heroes', route => {
+test.beforeEach(async ({ page }) => {
+  // mock heroes http request
+  await page.route('**/api/heroes', route => {
     route.fulfill({
       status: 200,
       body: JSON.stringify([
@@ -47,14 +23,14 @@ test('Success heroes HTTP fetch', async ({ page }) => {
       ]),
     });
   });
+});
 
-  await page.goto('http://localhost:5173/heroes');
+test('Success heroes HTTP fetch', async ({ page }) => {
+  await page.goto('/heroes');
 
-  // const a = page.locator('data-test-id=card');
-  // console.log(a);
-  // expect(await a.all()).toHaveLength(2);
+  const row1 = page.getByText('Barry Allen');
+  await expect(row1).toBeVisible();
 
-  // const b = page.getByTestId('card');
-  // console.log(b);
-  // expect(await b.all()).toHaveLength(2);
+  const row2 = page.getByText('Scott Summer');
+  await expect(row2).toBeVisible();
 });
