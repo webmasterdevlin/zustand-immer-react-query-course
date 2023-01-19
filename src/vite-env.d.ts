6aee47c4911cefb54fcaf8f2330cfc3027e590ac
devlin <webmasterdevlin@gmail.com>
2023-01-19 06:08:32 +0100

    update

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string;
+  readonly VITE_SOME_KEY: string;
+  readonly DB_PASSWORD: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
