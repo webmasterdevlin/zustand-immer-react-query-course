76d911df01cccf56a50687879733a5e07e082532
devlin <webmasterdevlin@gmail.com>
2023-02-20 11:23:48 +0100

    update

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -11,7 +11,6 @@ export default function Card({ children, ...rest }: Props) {
       className={'mb-4 rounded bg-slate-800 px-8 pt-6 pb-8 text-white shadow-md dark:bg-gray-100 dark:text-slate-900'}
       {...rest}
     >
-      ``
       {children}
     </div>
   );
