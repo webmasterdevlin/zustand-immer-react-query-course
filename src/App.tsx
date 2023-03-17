64a90b0f5e88bcc5c95b0c73fa2ea8b27e4b5db1
devlinduldulao <webmasterdevlin@gmail.com>
2023-03-17 22:22:23 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,11 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <NavigationBar />
-        <div className={'bg-white px-6 py-8 shadow-xl ring-1 ring-slate-900/5 dark:bg-slate-800 dark:text-white'}>
+        <div
+          className={
+            'prose bg-white px-6 py-8 shadow-xl ring-1 ring-slate-900/5 md:prose-lg lg:prose-xl dark:bg-slate-800 dark:text-white'
+          }
+        >
           <Routes />
         </div>
         <ReactQueryDevtools initialIsOpen={false} />
