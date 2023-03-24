6b43e058ea7cdaf43ab5f49d18ca947524e4bb64
devlinduldulao <webmasterdevlin@gmail.com>
2023-03-24 10:50:58 +0100

    update

--- src/components/FallbackRenderer.tsx
+++ src/components/FallbackRenderer.tsx
@@ -6,8 +6,8 @@ type Props = {
 export default function FallbackRenderer({ error }: Props) {
   return (
     <div role="alert" className="container prose mx-auto p-4">
-      <h1>Something went wrong:</h1>
-      <h3 className="text-red-800">{error.message}</h3>
+      <h1 className="dark:text-white">Something went wrong:</h1>
+      <h3 className="text-red-800 dark:text-orange-400">{error.message}</h3>
     </div>
   );
 }
