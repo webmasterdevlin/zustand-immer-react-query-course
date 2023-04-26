932b92c2622b43859a11a16594b5330260f64d2f
devlinduldulao <webmasterdevlin@gmail.com>
2023-04-26 12:42:25 +0200

    update

--- src/features/heroes/hooks/useAddHero.ts
+++ src/features/heroes/hooks/useAddHero.ts
@@ -8,33 +8,14 @@ export default function useAddHero() {
   const queryClient = useQueryClient();
 
   return useMutation(
-    hero => {
+    (hero: HeroModel) => {
       return postAxios<HeroModel>(EndPoints.heroes, hero);
     },
     {
-      onMutate: async (hero: HeroModel) => {
-        // Cancel any outgoing refetches (so they don't overwrite our optimistic update)
-        await queryClient.cancelQueries([keys.heroes]);
-
-        // Snapshot the previous value
-        const backup = queryClient.getQueryData<{ data: HeroModel[] }>([keys.heroes]);
-
-        // Optimistically update by adding the hero
-        if (backup)
-          queryClient.setQueryData<{ data: HeroModel[] }>([keys.heroes], {
-            data: [...backup.data, hero],
-          });
-
-        return { backup };
-      },
-
-      // If the mutation fails, use the context returned from onMutate to roll back
-      onError: (err, variables, context) => {
-        if (context?.backup) queryClient.setQueryData<HeroModel[]>([keys.heroes], context.backup.data);
-      },
-      // Always refetch after error or success:
-      onSettled: () => {
-        return queryClient.invalidateQueries([keys.heroes]);
+      onSuccess: res => {
+        queryClient.setQueryData<{ data: HeroModel[] }>([keys.heroes], cache => {
+          return cache?.data ? { data: [...cache.data, res.data] } : { data: [res.data] };
+        });
       },
     },
   );
