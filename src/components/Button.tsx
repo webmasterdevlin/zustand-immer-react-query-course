3c8701867a3c3e0f19ff2052f03aa6dfcc603833
Devlin Duldulao <webmasterdevlin@gmail.com>
2023-07-26 22:26:43 +0200

    update

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import React from 'react';
 
 type Props = {
@@ -12,25 +13,26 @@ type Props = {
 TailwindCSS because it will be easier to maintain and add new styles */
 
 export default function Button({ disabled, color = 'default', ...rest }: Props) {
-  let className = '';
+  const defaultClasses = 'mr-2 mb-2 rounded-lg px-5 py-2.5 text-center text-sm font-medium capitalize';
+  let colorClasses = '';
 
   switch (color) {
     case 'default':
-      className = `mr-2 mb-2 rounded-lg px-5 py-2.5 text-center text-sm font-medium capitalize text-black ${
-        disabled && 'text-gray-400'
-      }`;
+      colorClasses = `text-black ${disabled ? 'text-gray-400' : ''}`;
       break;
     case 'primary':
-      className = `mr-2 mb-2 rounded-lg px-5 py-2.5 text-center text-sm font-medium bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 capitalize text-white shadow-lg shadow-blue-500/50 hover:bg-gradient-to-br focus:outline-none focus:ring-4 focus:ring-blue-300 disabled:text-gray-300 dark:shadow-lg dark:shadow-blue-800/80 dark:focus:ring-blue-800  ${
-        disabled && 'text-gray-400'
+      colorClasses = `bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 text-white shadow-lg shadow-blue-500/50 hover:bg-gradient-to-br focus:outline-none focus:ring-4 focus:ring-blue-300 disabled:text-gray-300 dark:shadow-lg dark:shadow-blue-800/80 dark:focus:ring-blue-800  ${
+        disabled ? 'text-gray-400' : ''
       }`;
       break;
     case 'secondary':
-      className = `mr-2 mb-2 rounded-lg bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 px-5 py-2.5 text-center text-sm font-medium capitalize text-white shadow-lg shadow-purple-500/50 hover:bg-gradient-to-br focus:outline-none focus:ring-4 focus:ring-purple-300 disabled:text-gray-300 dark:shadow-lg dark:shadow-purple-800/80 dark:focus:ring-purple-800  ${
-        disabled && 'text-gray-400'
+      colorClasses = `bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 text-white shadow-lg shadow-purple-500/50 hover:bg-gradient-to-br focus:outline-none focus:ring-4 focus:ring-purple-300 disabled:text-gray-300 dark:shadow-lg dark:shadow-purple-800/80 dark:focus:ring-purple-800  ${
+        disabled ? 'text-gray-400' : ''
       }`;
       break;
   }
+
+  const className = clsx(defaultClasses, colorClasses, '', '');
 
   return (
     <button className={className} {...rest}>
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -8,7 +8,7 @@ type Props = {
 export default function Card({ children, ...rest }: Props) {
   return (
     <div
-      className={'mb-4 rounded bg-slate-800 px-8 pt-6 pb-8 text-white shadow-md dark:bg-gray-100 dark:text-slate-900'}
+      className={'mb-4 rounded bg-slate-800 px-8 pb-8 pt-6 text-white shadow-md dark:bg-gray-100 dark:text-slate-900'}
       {...rest}
     >
       {children}
