3b8afa5571af49d59d16e5e238e66b2c77a5efae
Devlin Duldulao <webmasterdevlin@gmail.com>
2023-10-13 13:16:50 +0200

    update

--- src/e2e/HomePage.test.ts
+++ src/e2e/HomePage.test.ts
@@ -2,5 +2,6 @@ import { expect, test } from '@playwright/test';
 
 test('Should have visible title message', async ({ page }) => {
   await page.goto('/');
-  expect(await page.textContent('h2')).toBe('Welcome to Zustand, Immer, and React Query Course 🧑‍🏫 💻');
+  const h2 = await page.textContent('h2');
+  expect(h2).toBe('Welcome to Zustand, Immer, and React Query Course 🧑‍🏫 💻');
 });
