f02e32fc1cdec789e054e04a52b5f6742ead2f35
Devlin Duldulao <webmasterdevlin@gmail.com>
2023-10-13 12:17:04 +0200

    update

--- src/test-utils/testing-library-utils.tsx
+++ src/test-utils/testing-library-utils.tsx
@@ -1,7 +1,9 @@
 // import '@testing-library/jest-dom/extend-expect';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { render as rtlRender } from '@testing-library/react';
+import { ErrorBoundary } from 'react-error-boundary';
 import { BrowserRouter } from 'react-router-dom';
+import FallbackRenderer from '../components/FallbackRenderer';
 import NavigationBar from '../components/NavigationBar';
 import type { QueryProviderWrapperProps } from './create-query-provider-wrapper';
 import type { ReactElement } from 'react';
@@ -15,7 +17,13 @@ const render = (ui: ReactElement, { ...renderOptions } = {}) => {
         <BrowserRouter>
           <>
             <NavigationBar />
-            <>{children}</>
+            <ErrorBoundary fallbackRender={FallbackRenderer}>
+              <div
+                className={' bg-white px-6 py-8 shadow-xl ring-1 ring-slate-900/5 dark:bg-slate-800 dark:text-white'}
+              >
+                {children}
+              </div>
+            </ErrorBoundary>
           </>
         </BrowserRouter>
       </QueryClientProvider>
