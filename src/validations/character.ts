46c69daebf11dfd60e5b4f60cae1a0eae8e3ce5f
Devlin Duldulao <webmasterdevlin@gmail.com>
2023-10-13 09:30:04 +0200

    update

--- src/validations/character.ts
+++ src/validations/character.ts
@@ -2,9 +2,9 @@ import { z } from 'zod';
 
 export const CharacterFormSchema = z.object({
   firstName: z.string().min(2, 'First Name must have at least 2 characters').max(20, 'Too many characters'),
-  lastName: z.string().min(2, 'Last Name must have at least 2 characters').max(20, 'Too many characters'),
   house: z.string().nullable(),
   knownAs: z.string().nullable(),
+  lastName: z.string().min(2, 'Last Name must have at least 2 characters').max(20, 'Too many characters'),
 });
 
 export type CharacterFormSchemaType = z.infer<typeof CharacterFormSchema>;
