97b76dcf2906622497931f404fa813a928847fc8
Devlin Duldulao <webmasterdevlin@gmail.com>
2024-02-21 19:29:21 +0100

    Update TanStackRouterDevtools position

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -22,12 +22,11 @@ function RootComponent() {
   return (
     <>
       <NavigationBar />
-
       <div className={' bg-white px-6 py-8 shadow-xl ring-1 ring-slate-900/5 dark:bg-slate-800 dark:text-white'}>
         <RouterSpinner />
         <Outlet />
       </div>
-      <TanStackRouterDevtools position="bottom-right" />
+      <TanStackRouterDevtools position="top-right" />
       <ReactQueryDevtools initialIsOpen={false} />
     </>
   );
