f3a5a49183b0b6bf6bd0f3f83faa50ec44e5fc31
Devlin Duldulao <webmasterdevlin@gmail.com>
2024-03-30 14:56:12 +0100

    Add @rollup/plugin-eslint to package.json

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
 /// <reference types="vite/client" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import eslint from 'vite-plugin-eslint';
+import eslint from '@rollup/plugin-eslint';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import million from 'million/compiler';
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
