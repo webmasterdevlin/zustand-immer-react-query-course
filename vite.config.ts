5119317c4210f21c4af01a028d92a5bdab5ef134
Devlin Duldulao <webmasterdevlin@gmail.com>
2024-04-02 16:00:31 +0200

    Remove million compiler from vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from '@rollup/plugin-eslint';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
-import million from 'million/compiler';
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths(), million.vite({ auto: true }), TanStackRouterVite()],
+  plugins: [react(), viteTsconfigPaths(), TanStackRouterVite()],
   test: {
     include: ['src/**/*.{test,spec}.{js,ts,tsx}'],
     exclude: ['src/e2e'],
