d62584d4c7fbdfe833a626365aba10bad937a4ee
Devlin Duldulao <webmasterdevlin@gmail.com>
2024-04-02 19:58:09 +0200

    Fix linting issues and update TypeScript configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,3 @@
-const fs = require('fs');
-const path = require('path');
-
-const prettierOptions = JSON.parse(fs.readFileSync(path.resolve(__dirname, '.prettierrc'), 'utf8'));
-
 module.exports = {
   env: {
     browser: true,
